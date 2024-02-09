2792e9fa1f7de94df9a2aa00a4c90b0ece5d9029
Kennan Guan <kennanguan722@gmail.com>
2024-02-09 14:12:09 -0500

    Set up methods

--- Grid.java
+++ Grid.java
@@ -1,5 +1,27 @@
 public class Grid{
-    public Grid() {
-        
+    Node[][] grid;
+    int size;
+
+    //Creates an n x n empty grid of nodes
+    public Grid(int n) {
+        size = n;
+        grid = new Node[size][size];
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++){
+                grid[i][j] = new Node();
+            }
+        }
     }
+
+    public void setObstacles() {
+
+    }
+
+    public void printGrid() {
+
+    }
+
+
+
 }
